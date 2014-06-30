73d45a200d42969b6c0c37f990dc63fd3c4492fb
DannyDesignx <mr.dannydesignx@gmail.com>
2014-06-30 18:06:40 +0000

    fuck this shiizzzzz

--- assets/www/js/application.js
+++ assets/www/js/application.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
-	$("#btn").on("click", function(){
-		$(this).addClass("cheese");
-		$(this).html("fuck oooooffff!");
-	});
-	
+$(document).ready(function() {
+    $("#btn").on("click", function() {
+        $(this).addClass("cheese");
+        $(this).html("fuck oooooffff!");
+    });
 });
